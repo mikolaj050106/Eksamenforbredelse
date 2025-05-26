d675b8297b5a0362826e798ff856e8452a3c7251
mikolaj050106 <148052337+mikolaj050106@users.noreply.github.com>
2025-05-26 10:16:29 +0200

    2nd commit

--- kode/main.py
+++ kode/main.py
@@ -16,7 +16,7 @@ def legg_til_bruker():
     navn = input("Skriv inn navn")
     pc_nummer = input("skriv inn PC-nummer")
     cursor.excecute("INSERT INTO brukere (navn, pc_nummer) VALUES (?,?)", (navn, pc_nummer))
-    con.commit()
+    conn.commit()
     print("Bruker lagt til!")
 
 def vis_brukere():
@@ -24,3 +24,18 @@ def vis_brukere():
     for rad in cursor.fetchall():
         print(f"ID: {rad[0]}, Navn: {rad[1]}, PC-nummer: {rad[2]}")
 
+def meny():
+    while True:
+        print("\n1. Legg til bruker\n2. Vis alle brukere\n3. Avslutt")
+        valg = input("Velg et alternativ: ")
+        if valg == "1":
+            legg_til_bruker()
+        elif valg == "2":
+            vis_brukere()
+        elif valg == "3":
+            break
+        else: 
+            print("Ugyldig valg, prøv igjen.")
+
+meny()
+conn.close()
