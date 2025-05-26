8603e54ef223bf02103499d70265fd46df78f71b
mikolaj050106 <148052337+mikolaj050106@users.noreply.github.com>
2025-05-26 22:10:00 +0200

    test

---
+++ kode/app.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, render_template_string 
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET, POST"])
+def index():
+    conn = sqlite3.connect("brukere.db")
+    cursor = conn.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXIST brukere (navn TEXT, pcnummer TEXT)")
+
+    if request.method == "POST":
+        navn = request.form["navn"]
+        pcnummer = request.form["pcnummer"]
+        cursor.execute("INSERT INTO brukere (navn, pcnummer) VALUES (?, ?)", (navn, pcnummer))
+        conn.commit()
+
+    cursor.execute("SELECT * FROM brukere")
+    brukere = cursor.fetchall()
+    conn.close()
+
+    return render_template_string('''
+    <h2>Registrer bruker</h2>
+    <form method="POST">
+        Navn: <input type="text" name="navn"><br>
+        PC-nummer: <input type="text" name="pcnummer"><br>
+        <input type="submit" value="Registrer">
+    </form>
+    <h3>Registrerte brukere:</h3>
+    <ul>
+        {% for navn, pcnummer in brukere %}
+            <li>{{ navn }} – {{ pcnummer }}</li>
+        {% endfor %}
+    </ul>
+    ''', brukere=brukere)
+
+if __name__ == "__main__":
+    app.run(debug=True)
